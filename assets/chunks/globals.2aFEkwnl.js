1b2b26a71d122d0369322017d971f2c8f4f862a0
xh-shen <9509394@qq.com>
2025-09-19 08:33:16 +0000

    Deploying to main from @ pro-design-vue/pro-design@997a64dcac282e902236f2e9ba7d5a07790a896e 🚀

--- assets/chunks/globals.2aFEkwnl.js
+++
@@ -1,1 +0,0 @@
-const _="--pro-content-height",o="--pro-content-width",t="--pro-header-height",E="--pro-footer-height",T="__pro_main_content",A="_scroll__fixed_";export{_ as C,T as E,A as S,o as a,t as b,E as c};
